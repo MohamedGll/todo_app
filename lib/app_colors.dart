3af5674b65917bd9b86ff68399e06adcc57931f8
khorshed <mg6914949@gmail.com>
2024-08-02 18:53:59 +0300

    create splash view

---
+++ lib/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = const Color(0xff5D9CEC);
+  static Color secondary = const Color(0xffDFECDB);
+  static Color grey = const Color(0xffC8C9CB);
+}
---
+++ lib/views/splash_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/app_colors.dart';
+import 'package:todo_app/views/home_view.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+  static const String id = 'SplashView';
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(
+        seconds: 3,
+      ),
+      () {
+        Navigator.pushNamed(context, HomeView.id);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.secondary,
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+        ),
+      ),
+    );
+  }
+}
