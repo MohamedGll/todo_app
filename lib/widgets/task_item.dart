dbdebf642830c66f250f7e9b414c7dfa7ec9418f
khorshed <mg6914949@gmail.com>
2024-08-03 04:03:57 +0300

    use slidable in task item

--- lib/widgets/task_item.dart
+++ lib/widgets/task_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:todo_app/app_colors.dart';
 
 class TaskItem extends StatelessWidget {
@@ -6,70 +7,94 @@ class TaskItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 115,
-      width: double.infinity,
-      margin: const EdgeInsets.symmetric(horizontal: 24),
-      padding: const EdgeInsets.all(18),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Row(
+    return Slidable(
+      startActionPane: ActionPane(
+        motion: const DrawerMotion(),
+        extentRatio: .44,
         children: [
-          Container(
-            height: 70,
-            width: 4,
-            decoration: BoxDecoration(
-              color: AppColors.primary,
-              borderRadius: BorderRadius.circular(12),
-            ),
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: const Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            icon: Icons.delete,
+            label: 'Delete',
           ),
-          const Spacer(
-            flex: 1,
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: const Color(0xFF21B7CA),
+            foregroundColor: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            icon: Icons.edit,
+            label: 'Edit',
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Play basket ball',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700,
-                  color: AppColors.primary,
-                ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              const Text(
-                '10:30 AM',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
-          const Spacer(
-            flex: 2,
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: AppColors.primary,
-              shape: RoundedRectangleBorder(
+        ],
+      ),
+      child: Container(
+        height: 115,
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(horizontal: 24),
+        padding: const EdgeInsets.all(18),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          children: [
+            Container(
+              height: 70,
+              width: 4,
+              decoration: BoxDecoration(
+                color: AppColors.primary,
                 borderRadius: BorderRadius.circular(12),
               ),
             ),
-            onPressed: () {},
-            child: const Icon(
-              Icons.done,
-              color: Colors.white,
+            const Spacer(
+              flex: 1,
             ),
-          ),
-        ],
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Play basket ball',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: AppColors.primary,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const Text(
+                  '10:30 AM',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(
+              flex: 2,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () {},
+              child: const Icon(
+                Icons.done,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
