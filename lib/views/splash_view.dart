def3a2448c79996166c5c1bd9e41abff584bae63
khorshed <mg6914949@gmail.com>
2024-08-04 22:39:26 +0300

    app theme done

--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_app/app_colors.dart';
+import 'package:todo_app/providers/theme_provider.dart';
 import 'package:todo_app/views/home_view.dart';
 
 class SplashView extends StatefulWidget {
@@ -26,8 +28,11 @@ class _SplashViewState extends State<SplashView> {
 
   @override
   Widget build(BuildContext context) {
+    var themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
-      backgroundColor: AppColors.secondary,
+      backgroundColor: themeProvider.appTheme == ThemeMode.dark
+          ? AppColors.primaryDark
+          : AppColors.primary,
       body: Center(
         child: Image.asset(
           'assets/images/logo.png',
